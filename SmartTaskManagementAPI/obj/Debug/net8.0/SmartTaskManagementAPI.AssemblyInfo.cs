78b55931ba51b8a5572645f34609b4c74e64d375
Tajudeen Busari <53045507+TajudeenBusari@users.noreply.github.com>
2024-11-04 21:54:24 +0200

    task-category CRUD done! Close #2

--- SmartTaskManagementAPI/obj/Debug/net8.0/SmartTaskManagementAPI.AssemblyInfo.cs
+++ SmartTaskManagementAPI/obj/Debug/net8.0/SmartTaskManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e91aba4f1c242507e09346c4c8049222fc5937b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49bff9447464b56ebcdaa477065b95983081d7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
