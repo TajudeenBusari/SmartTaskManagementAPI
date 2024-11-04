c80531031538c2f6d937bf36c6720b997f173f10
Tajudeen Busari <53045507+TajudeenBusari@users.noreply.github.com>
2024-11-04 22:06:23 +0200

    resolving1

--- SmartTaskManagementAPI/obj/Debug/net8.0/SmartTaskManagementAPI.AssemblyInfo.cs
+++ SmartTaskManagementAPI/obj/Debug/net8.0/SmartTaskManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84e6e67ea934a8085331d0f8fd6778b15f348804")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295165e20cd06b97fdc36a0dd7cc1cfcb4d663c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
