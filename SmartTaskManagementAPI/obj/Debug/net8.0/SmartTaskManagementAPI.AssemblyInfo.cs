a2e20a6cc2b810af3b052f70e13609048f29d220
Tajudeen Busari <53045507+TajudeenBusari@users.noreply.github.com>
2024-11-04 22:02:58 +0200

    resolving merge conflict3

--- SmartTaskManagementAPI/obj/Debug/net8.0/SmartTaskManagementAPI.AssemblyInfo.cs
+++ SmartTaskManagementAPI/obj/Debug/net8.0/SmartTaskManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e047b81bda30bd64be5d1b86ba3efee38c13df49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b970870fc28084777c2d4e84379cdd1e955e7bc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartTaskManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
